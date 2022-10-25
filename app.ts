932fc547d867baf39c7793f455d7f22e563df669
Osamu Takiya <takiya@toran.sakura.ne.jp>
2022-10-25 10:21:53 +0900

    feat: 🎸 出力する JSON を Pretty にした
    close #13

--- app.ts
+++ app.ts
@@ -2,13 +2,14 @@ import { FetchKickstarterPledges } from './FetchKickstarterPledges'
 import * as fs from 'fs'
 
 async function main() {
+  // https://www.kickstarter.com/projects/rabbitandbearstudios/eiyuden-chronicle-hundred-heroes/description
   const pledgesData = await new FetchKickstarterPledges(
     'rabbitandbearstudios',
     'eiyuden-chronicle-hundred-heroes'
   ).exec()
 
   const outputFilepath = './tmp/pledges.json'
-  fs.writeFileSync(outputFilepath, JSON.stringify(pledgesData))
+  fs.writeFileSync(outputFilepath, JSON.stringify(pledgesData, null, 2))
 }
 
 main()
