4b88e1b17d8ea3b52b5a4db727c1df2e0290f57d
Osamu Takiya <takiya@toran.sakura.ne.jp>
2023-05-03 12:58:36 +0900

    feat: 🎸 クリエイター名 および プロジェクト名 および 出力ファイルパス を引数で指定できるようにした (#123)

--- app.ts
+++ app.ts
@@ -2,14 +2,30 @@ import { FetchKickstarterPledges } from './FetchKickstarterPledges'
 import * as fs from 'fs'
 
 async function main() {
+  const firstCliArgv = process.argv[2]
+  const secondCliArgv = process.argv[3]
+
+  if (!firstCliArgv || !secondCliArgv) {
+    console.log('Please input creator and project.')
+
+    return
+  }
+
   // https://www.kickstarter.com/projects/rabbitandbearstudios/eiyuden-chronicle-hundred-heroes/description
   const pledgesData = await new FetchKickstarterPledges(
-    'rabbitandbearstudios',
-    'eiyuden-chronicle-hundred-heroes'
+    firstCliArgv, // Creator
+    secondCliArgv // Project
   ).exec()
 
-  const outputFilepath = './tmp/pledges.json'
+  const outputFilepath = process.argv[4] || './tmp/pledges.json'
   fs.writeFileSync(outputFilepath, JSON.stringify(pledgesData, null, 2))
+
+  console.log('[LOG] Done:')
+  console.log(`Output: ${outputFilepath}`)
+  console.log(`Creator: ${firstCliArgv}, Project: ${secondCliArgv}`)
+  console.log(
+    `URL: https://www.kickstarter.com/projects/${firstCliArgv}/${secondCliArgv}/description`
+  )
 }
 
 main()
